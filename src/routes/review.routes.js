babd7c19e83709c6b762bc15d55e7f75783bdb29
Bilal-billu <bilalmalikoff@gmail.com>
2025-08-12 14:28:09 +0500

    added a new api to allow user to edit their reviews

--- src/routes/review.routes.js
+++ src/routes/review.routes.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import { verifyJWTToken } from "../middleware/userAuth.middleware.js";
-import { postNewReview } from "../controllers/review.controllers.js";
+import { postNewReview, updateReview } from "../controllers/review.controllers.js";
 
 const reviewRouter = Router();
 reviewRouter.route('/new-review').post(
     verifyJWTToken,
     postNewReview
 );
+
+reviewRouter.route('/update-review/:id').patch(
+    verifyJWTToken,
+    updateReview
+)
 
 
 export default reviewRouter
